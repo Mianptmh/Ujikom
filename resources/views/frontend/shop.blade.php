b13e19fa30fe4257a485eca63a0977528a1ea46f
Yusups20 <yups2092003@gmail.com>
2020-01-14 12:03:04 +0700

    login

---
+++ resources/views/frontend/shop.blade.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Mianptmh &mdash; Website Template by Colorlib</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <link href="https://fonts.googleapis.com/css?family=Cinzel:400,700|Montserrat:400,700|Roboto&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="frontend/login/css/bootstrap.min.css">
+  <link rel="stylesheet" href="frontend/login/css/jquery-ui.css">
+  <link rel="stylesheet" href="frontend/login/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="frontend/login/css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="frontend/login/css/owl.theme.default.min.css">
+
+  <link rel="stylesheet" href="frontend/login/css/jquery.fancybox.min.css">
+
+  <link rel="stylesheet" href="frontend/login/css/bootstrap-datepicker.css">
+
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+  <link rel="stylesheet" href="frontend/login/css/aos.css">
+  <link href="frontend/login/css/jquery.mb.YTPlayer.min.css" media="all" rel="stylesheet" type="text/css">
+
+  <link rel="stylesheet" href="frontend/login/css/style.css">
+
+
+
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+
+
+
+    <div class="header-top">
+        <div class="container">
+          <div class="row align-items-center">
+                  <div class="col-md-8">
+                      <h1>AYAMKU</h1>
+                  </div>
+            <a href="#" class="mx-auto d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                  class="icon-menu h3"></span></a>
+          </div>
+        </div>
+
+
+
+
+      <div class="site-navbar py-2 js-sticky-header site-navbar-target d-none pl-0 d-lg-block" role="banner">
+
+      <div class="container">
+        <div class="d-flex align-items-center">
+
+          <div class="mx-auto">
+            <nav class="site-navigation position-relative text-left" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mx-auto d-none pl-0 d-lg-block border-none">
+                <li><a href="./index" class="nav-link text-left">Home</a></li>
+                <li><a href="./about" class="nav-link text-left">About</a></li>
+                <li><a href="./ayamku" class="nav-link text-left">Ayamku</a></li>
+                <li class="active"><a href="./shop" class="nav-link text-left">Shop</a></li>
+                <li><a href="./contact" class="nav-link text-left">Contact</a></li>
+              </ul>
+            </nav>
+
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    </div>
+
+
+
+
+
+
+
+    <div class="site-section mt-5">
+      <div class="container">
+
+        <div class="row mb-5">
+          <div class="col-12 section-title text-center mb-5">
+            <h2 class="d-block">Our Products</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, perspiciatis!</p>
+          </div>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 mb-5 col-md-6">
+
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/1.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block">Rp.200.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/2.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.200.0000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block"><del>Rp900.000</del> Rp.200.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/3.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block"><del>rp.900.000</del> Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 mb-5 col-md-6">
+
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/4.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block">Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/5.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block"><del>Rp900.000</del> Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/6.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block"><del>Rp900.000</del> Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 mb-5 col-md-6">
+
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/7.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block">Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/8.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block"><del>Rp900.000</del> Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+              <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="frontend/login/images/9.jpg" alt="Image" class="img-fluid"></a>
+              <div>
+                <h3 class="heading mb-1"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price">Rp.150.000</span>
+              </div>
+
+
+              <div class="wine-actions">
+
+                <h3 class="heading-2"><a href="#">Trius Cabernet Indonesia 2020</a></h3>
+                <span class="price d-block"><del>Rp900.000</del> Rp.150.000</span>
+
+                <div class="rating">
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star"></span>
+                  <span class="icon-star-o"></span>
+                </div>
+
+                <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span> Add to Cart</a>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+
+    <div class="hero-2" style="background-image: url('images/hero_2.jpg');">
+     <div class="container">
+        <div class="row justify-content-center text-center align-items-center">
+          <div class="col-md-8">
+            <span class="sub-title">Welcome</span>
+            <h2>Chicken For Everyone</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section bg-light">
+        <div class="container">
+          <div class="owl-carousel owl-slide-3 owl-slide">
+
+            <blockquote class="testimony">
+              <img src="frontend/login/images/pelung.jpg" alt="Image">
+              <p>&ldquo;Setelah itu, klik untuk Anda lihat, dan Anda Dapat Menemukannya.&rdquo;</p>
+              <p class="small text-primary">&mdash; Ayam Pelung</p>
+            </blockquote>
+            <blockquote class="testimony">
+              <img src="frontend/login/images/kate.jpg" alt="Image">
+              <p>&ldquo;Setelah itu, klik untuk Anda lihat, dan Anda Dapat Menemukannya.&rdquo;</p>
+              <p class="small text-primary">&mdash; Ayam Kate</p>
+            </blockquote>
+            <blockquote class="testimony">
+              <img src="frontend/login/images/balenggek.jpg" alt="Image">
+              <p>&ldquo;Setelah itu, klik untuk Anda lihat, dan Anda Dapat Menemukannya.&rdquo;</p>
+              <p class="small text-primary">&mdash; Ayam Balenggek</p>
+            </blockquote>
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+    <div class="footer">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-12 text-center">
+            <div class="social-icons">
+              <a href="#"><span class="icon-facebook"></span></a>
+              <a href="#"><span class="icon-twitter"></span></a>
+              <a href="#"><span class="icon-youtube"></span></a>
+              <a href="#"><span class="icon-instagram"></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="copyright">
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    </p>
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+  <!-- .site-wrap -->
+
+
+  <!-- loader -->
+  <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#ff5e15"/></svg></div>
+
+  <script src="frontend/login/js/jquery-3.3.1.min.js"></script>
+  <script src="frontend/login/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="frontend/login/js/jquery-ui.js"></script>
+  <script src="frontend/login/js/popper.min.js"></script>
+  <script src="frontend/login/js/bootstrap.min.js"></script>
+  <script src="frontend/login/js/owl.carousel.min.js"></script>
+  <script src="frontend/login/js/jquery.stellar.min.js"></script>
+  <script src="frontend/login/js/jquery.countdown.min.js"></script>
+  <script src="frontend/login/js/bootstrap-datepicker.min.js"></script>
+  <script src="frontend/login/js/jquery.easing.1.3.js"></script>
+  <script src="frontend/login/js/aos.js"></script>
+  <script src="frontend/login/js/jquery.fancybox.min.js"></script>
+  <script src="frontend/login/js/jquery.sticky.js"></script>
+  <script src="frontend/login/js/jquery.mb.YTPlayer.min.js"></script>
+
+
+
+
+  <script src="frontend/login/js/main.js"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,26 @@ Route::get('/index', function () {
     return view('frontend.index');
 });
 
+Route::get('/about', function () {
+    return view('frontend.about');
+});
+
+Route::get('/contact', function () {
+    return view('frontend.contact');
+});
+
+Route::get('/shop', function () {
+    return view('frontend.shop');
+});
+
+Route::get('/ayamku', function () {
+    return view('frontend.ayamku');
+});
+
+Route::get('/cart', function () {
+    return view('frontend.cart');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
